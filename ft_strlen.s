1d211d688d622ebd6d45b8a8dc39ada3f7b5f05a
waelzahir <minimala978@gmail.com>
2025-10-18 19:32:36 +0100

    strlen  done

--- ft_strlen.s
+++ ft_strlen.s
@@ -3,13 +3,15 @@ section .text
 global ft_strlen
 
 ft_strlen:
-    mov r8 ,rdi
-    start_loop:
-        cmp byte [r8], 0
-        je exit_loop
-        inc r8
-        jne start_loop
-    exit_loop:
-    mov rax, r8 
-    sub rax, rdi
+    push rdi 
+    loop:
+    cmp byte [rdi], 0
+    je exit
+    inc rdi
+    jmp loop
+    exit: 
+    pop rsi 
+    mov rax , rdi 
+    sub rax , rsi 
     ret
+     
