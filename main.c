5987f7a3834a4ef93d042d5b5b9ece28aedc1256
Ouail Zahir <ozahir@e2r9p1.1337.ma>
2025-11-04 22:36:43 +0100

    error in duplication check

--- main.c
+++ main.c
@@ -10,48 +10,12 @@
 
 int main(){
 
-    // // ft_read
-    // char e1[10] ;
-    // ft_read(0, e1, 10);
-    // printf("%s lenght", e1);
-    // printf("%ld\n", strlen(e1));
-
-    /// ft_strcmp
-    char e2[10] = "123456789";
-    char e3[10] = "123456789";
-    printf("def strcmp %d mimiced one %d\n", strcmp(e2,e3) ,ft_strcmp(e2, e3));
-
-
-    // ft_strcpy
-    char mstring[] = "hello world";
-    char *e4 = malloc(20);
-    char *e5 = malloc(20);
-    printf("%ld \n", strlen(mstring));
-    printf("def function string [%s] \n", strcpy(e4, mstring));
-    printf("mimiced function string [%s] \n", ft_strcpy(e5, mstring));
-    printf("default lenght [%ld]  mimiced lenght [%ld]\n",  strlen(e4), strlen(e5));
-    
-    
-    // ft_strdup
-    char *e6 = "blend jj";
-    char *dup = strdup(e6);
-    char *ft_dup = ft_strdup(e6);
-    printf("def function string [%s] \n", dup);
-    printf("mimiced function string [%s] \n", ft_dup);
-    printf("default lenght [%ld]  mimiced lenght [%ld]\n",  strlen(dup), strlen(ft_dup));
-    
-    // ft_strlen
-     char *e7 = "123";
-     int lenght  = ft_strlen(e7);
-     printf("%d\n", lenght);
-    
-    // ft_read
-    ft_write( -1, "e\n", 2);
-    perror("");
-
+   
     // putnmbrbase
-    int ret = ft_putnbr_base(44, "an47");
-    printf("%d\n", ret);
+     char *str = "12";
+    int ret = ft_putnbr_base(-55, str);
+    printf(" \n%d\n%d\n", ret, (int) (str));
+    perror("");
 
 
 
--- src/ft_putnbr_base.s
+++ src/ft_putnbr_base.s
@@ -1,77 +1,140 @@
 global ft_putnbr_base
 extern ft_strlen
+extern ft_write
 
 ; r12 number to be inputed
 
 ft_putnbr_base:
-;    check if string is null
-    cmp rsi , 0
-    je quit
-
-; ; check if lenght is more than one, require switching rsi with rdi 
-; ; they will be reverser for sanity pupoose and reciver when done
-    push rdi 
-    mov rdi, rsi
+    cmp rsi, 0
+    je exit_fail;
+    push rdi
+    push rsi 
+    mov rdi ,rsi
     call ft_strlen
+    pop rsi 
     pop rdi
-    cmp rax, 1
-        jbe quit
+    cmp rax, 2
+    jl exit_fail
+    mov r10, rsi 
+    loop:
+        cmp byte[r10], '-'
+        je exit_fail
+        cmp byte[r10], '+'
+        je exit_fail
+        inc r10
+        cmp byte[r10], 0
+        je check_success
+        jmp loop
+check_success:
+    cmp edi , 0
+    jge check_duplicates
+    neg edi
 
-; ; allocate 256 bytes in the stack and zero them
 
-    sub rsp , 256
-    mov r10, rsp 
-    mov r11, 32
+
+print_negative:
+    push rax
+    push rdi
+    push rsi
+    sub rsp, 1
+    mov byte[rsp], '-'
+    mov rdi , 1
+    mov rsi, rsp
+    mov rdx, 1
+    call ft_write;
+    add rsp, 1
+    pop rsi 
+    pop rdi
+    mov r10, rax
+    pop rax
+    cmp r10 , 0
+    jl exit_fail
+  
+
+check_duplicates:
+   
+    sub rsp, 256
+    mov r11, 0
     zero:
-        mov qword [r10], 0
-        add r10 , 8
-        dec r11
-        jnz zero
-
-
-; check if duplicate exists
-
-    mov r10, rsp ; baseof map
-    mov r11 , 0 ; r10 index
-    ; rsi string
-    check:
-        movzx r13, byte [rsi + r11]
-        cmp r13, 0
-        je continue
-        cmp r13, '-'
-        je check_fail
-        cmp r13, '+'
-        je check_fail
-        cmp  byte [r10 + r13], 1
-        je check_fail
-        mov  byte [r10 + r13], 1
+        mov r10b, byte [rsi +r11]
+        mov byte [rsp + r10], 0
         inc r11
-        jmp check
+        cmp byte [rsi, + r10], 0
+        je check_body
+        jmp zero
+check_body:
+    
+    mov r11, 0
+    check_loop:
+        mov r10b, byte [rsi +r11]
+        cmp r10b, 0
+        je no_duplicates
+        cmp byte [rsp , r10], 0
+        jne check_fail
+        mov byte [rsp +r10], 1
+        inc r11
+        jmp check_loop
 
-    continue:
+no_duplicates:
+    
+    ;recover stack to its original position
     add rsp, 256
-    mov rcx, rax
-    inc rcx
-    sub rsp, rcx
-    mov r10, rax ;  lenght
-    mov rax, rdi 
-    mov r12, rsi
-    mov r13 , rax
+    
+    ; copy stackPointer
+    mov r11 , rsp 
+    mov r10d, eax 
+    ; ; r10d hold string lengh
+    ; ; rdi hold the integer
+    ; ; rsi hold the string 
+        mov eax, edi
+    modulo_loop:
+        cdq
+        idiv r10d 
+        push rdx
+        cmp  eax , 0
+        pop rdx
+    mov rax, 222222
+    ret 
+    ;     je print_pop;
+    ;     jmp modulo_loop
+    ; mov r10 , rsi
+    ; print_pop:
+    ; mov rax , rsi
+    ; ret 
+    ; ;     cmp rsp , r11
+    ; ;     je  func_success
+    ; ;     mov rdi , 1
+    ; ;     mov r12b, [rsp];
+    ; ;     add r12, r10
+    ; ;     mov rsi , r12 
+    ; ;     mov rdx , 1
+    ; ;     call ft_write
+    ; ;     pop rdx
+    ; ;     jmp print_pop
+    ; ; ; mov eax ,r10d
 
-    loop_div;
-    div 
-
+func_success
     ret
+
+
+
+
+
+
 
 check_fail:
     add rsp, 256
+    jmp exit_fail
+
+    
+
+
+
+exit_fail:
     mov rax, -1
     ret
 
-quit:
-    mov rax, -1
-    ret
+  
 
 
-
-
+  section .note.GNU-stack
