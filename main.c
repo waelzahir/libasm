0a1f62d30cf1c5cfbc7e2e96937051a5c033593c
waelzahir <minimala978@gmail.com>
2025-11-12 22:08:33 +0100

    list sort done with the laziest algorithm

--- main.c
+++ main.c
@@ -4,11 +4,90 @@
 #include <stdlib.h>
 #include "headers/libasm.h"
 
-void check_ptr(void *p)
-{
-    printf("whats here %p \n", p);
+// void check_ptr(void *p)
+// {
+//     printf("whats here %p \n", p);
 
-}
+// }
+
+
+// int testFunc()
+// {
+//     printf("mamak\n");
+//     return 2;
+// }
+// int ft_list_sort(t_list **begin_list, int (*cmp)()){
+//     t_list **head = begin_list;
+//     t_list *first = *(begin_list);
+//     t_list *second = NULL;
+//     int it = 0;
+//     if (!first)
+//         return 1;
+//     second = first->next;
+//     if (!second)
+//         return 1;
+//     printf("here > \n");
+//     while(1)
+//     {
+//         // if (cmp(first->data, second->data))
+//         //     node_swap(begin_list, it, first,)
+//         printf("%s %s %s   itertion %d \n", (*head)->data , first->data,second->data, it );
+//         head = &first->next;
+//         first = second;
+//         second = second->next;
+//         if (!second)
+//             break;
+//     }
+//     return 1;
+// }
+// int main(){
+//     t_list * first_node  = calloc(1, sizeof(t_list));
+//     t_list * second_node  = calloc(1, sizeof(t_list));
+//     t_list * third_node  = calloc(1, sizeof(t_list));
+//     t_list * fourth_node  = calloc(1, sizeof(t_list));
+
+
+//     char *s1 = "hello";
+//     char * s2 = "world"; 
+//     char * s3 = "mamak"; 
+//     char *s4 = "mamamamm";
+
+//     first_node->data = s1;
+//     first_node->next = second_node;
+//     second_node-> data = s2;
+//     second_node->next = third_node;
+//     third_node->data = s3;
+//     third_node->next = NULL;
+
+   
+//    ft_list_sort(&first_node, NULL);    
+// }
+
+
+
+
+
+
+\
+\
+\
+\
+\
+\
+\
+\
+// void travers_list(t_list **head)
+// {
+//     t_list * h = (*head);
+//     while(h)
+//     {
+//         printf("text in node: %s  \n",  h->data);
+//         h = h->next;
+//         printf("%p\n", h);
+//     }
+// }
+
+
 
 void travers_list(t_list **head)
 {
@@ -17,33 +96,68 @@ void travers_list(t_list **head)
     {
         printf("text in node: %s  \n",  h->data);
         h = h->next;
-        printf("%p\n", h);
     }
 }
 
-int testFunc()
-{
-    printf("mamak\n");
-    return 2;
-}
+
 int main(){
-    t_list * first_node  = calloc(1, sizeof(t_list));
+    t_list * n1  = calloc(1, sizeof(t_list));
+    t_list * n2  = calloc(1, sizeof(t_list));
+    t_list * n3  = calloc(1, sizeof(t_list));
+    t_list * n4  = calloc(1, sizeof(t_list));
+    t_list * n5  = calloc(1, sizeof(t_list));
+    t_list * n6  = calloc(1, sizeof(t_list));
+    t_list * n7  = calloc(1, sizeof(t_list));
+    t_list * n8  = calloc(1, sizeof(t_list));
+    t_list * n9  = calloc(1, sizeof(t_list));
+    t_list * n10  = calloc(1, sizeof(t_list));
 
-    char *s1 = "hello";
-    char * s2 = "world"; 
-    char * s3 = "mamak"; 
-
-    first_node->data = s1;
-   
-   ft_list_sort(&first_node, &testFunc);    
-}
 
 
-void ft_list_sort(t_list **begin_list, int (*cmp)()){
-    t_list **ptr = begin_list;
+    char *s1 = "9";
+    char * s2 = "8"; 
+    char * s3 = "8"; 
+    char *s4 = "2";
+    char *s5 = "6";
+    char * s6 = "9"; 
+    char * s7 = "8"; 
+    char *s8 = "2";
+    char *s9 = "6";
+    char * s10 = "9"; 
+
+
+    n1->data = s1;
+    n2->data = s2;
+    n3->data = s3;
+    n4->data = s4;
+    n5->data = s5;
+    n6->data = s6;
+    n7->data = s7;
+    n8->data = s8;
+    n9->data = s9;
+    n10->data = s10;
     
+    n1->next = n2;
+    n2->next = NULL;
+    n3->next = NULL;
+    n4->next = n5;
+    n5->next = n6;
+    n6->next = n7;
+    n7->next = n8;
+    n8->next = n9;
+    n9->next = n10;
+    n10->next = NULL;
+
+   int ret = ft_list_sort(&n1, &ft_strcmp);    
+   printf("%d %d \n", n1->data, ret);
+   travers_list(&n1);
 
 }
+
+
+
+
+
 
 
 
--- src/ft_list_sort.s
+++ src/ft_list_sort.s
@@ -1,11 +1,50 @@
 global ft_list_sort
+extern ft_write
 
 ft_list_sort:
-    ;root hook
-    mov r10, rdi
-    ; cmp function
-    mov r11, rsi
+    mov r14,  rdi 
+    mov r15 , rsi
+    mov rsi , [rdi]
+    loop: 
+        mov rdi , rsi
+        cmp rdi, 0
+        je exit_success
+        mov rsi, [rdi + 8]
+        cmp rsi, 0
+        je exit_success
+        push rdi
+        push rsi 
+        mov rdi, [rdi]
+        mov rsi, [rsi]
+        call r15 
+        pop rsi
+        pop rdi 
+        cmp eax, 0
+        jg swap_sym
+        jmp loop
 
-    ; rsi  comparizon finction
+swap_sym: 
+    
+    ;stack adress 
+    mov r10, [r14]
+    ; second node next pointer 
+    mov r12, [rsi + 8]
+    ;link first node with node2->next
+    mov [rdi +8], r12
+    ; node2->next = head
+    mov [rsi + 8], r10
+    ;move node2 to head 
+    mov [r14], rsi
+    jmp loop
+
+exit_success:
+    mov rax, 1
+    ret
 
 section .note.GNU-stack
+
+
+
+
+
+
