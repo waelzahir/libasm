b1200c02b46cba31909be466a57df50ec4271aaf
waelzahir <minimala978@gmail.com>
2025-11-16 04:58:18 +0100

    wa l3adaw

--- src/ft_list_remove_if.s
+++ src/ft_list_remove_if.s
@@ -4,56 +4,60 @@ global ft_list_remove_if
 ft_list_remove_if:
   ;rdi ** of the head
   ; rsi value to pass to 
- 
+
+
+
+
+
   mov r11 , rdi
-  mov r12, rdi
-  mov r13, [r12]
+  mov r12, rdi ; hook
+  mov r13, [r12] ; node
   mov rax ,r13
   Label0:
    
-    mov r10, rdx
-    mov rdi, [r13]
+    mov r10, rdx ; function to call
+    mov rdi, [r13] ; data pointer
     call call_func
-   
-
     cmp rax , 0
-    je Label_del  
+    je Label_del  ; node to be deleted 
+    
+    mov r12, r13 ; new hook in r12
     Label_con:
-    mov r12, r13
-    mov r13, [r13 + 8]
-    cmp r13, 0
+    mov r13, [r12 + 8] ; new node in r13
+    cmp r13, 0; check if node is 0 to exit 
     je Label_exit
-    
-    
     jmp Label0
 
 
+Label_del:
+  mov r10 , rcx 
+  call call_func
+  mov r10 ,[r13 +8]
+  cmp r11 , r12
+  je label_head
+  jmp Label_con
+
+    
+
+
+
+  label_head:  
+  mov [r11], r10
+  mov rdi, r11
+  jmp ft_list_remove_if
 
 
 
 Label_exit:
-  mov rax, -1
+  
+  mov rax, r11
   ret 
 
 
 
 
-Label_del:
-  mov r10, rcx
-  call call_func
-  
-  
-  mov r10 ,[r13 + 8]
-  mov [r12 + 8], r10
-  cmp r11, r12 
-  jne Label_skip
-  mov r13, r12
-  jmp Label_con
-  Label_skip:
-  mov rax , r11
-  ret
-  jmp Label_con
-  
+
+
 call_func:
   push r11 
   push rdi
